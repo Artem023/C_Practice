cf49bc13c725cf5643a79b4545c77086d32d2400
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-20 17:15:45 +0300

    Done my HW and added all task (009-016)

---
+++ Task009/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу, которая: 
+// 1. выводит случайное число из отрезка [10, 99]  
+// 2. показывает наибольшую цифру числа.
+
+// 1.
+int number = new Random().Next(10, 100); // Программа для вывода случайного числа
+Console.WriteLine($"Случайное число из отрезка 10 - 99 --> {number}");
+
+// 2.
+int firstDigit = number / 10; 
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа --> {firstDigit} ");
+else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit} ");
+
+// Здесь есть встроенный метод, который определяет наибольшую цифру числа:
+Console.WriteLine($"Наибольшая цифра числа --> {Math.Max(firstDigit, secondDigit)} ");
+
+// Далее реализуем задачу с помощью функции (методов)
+int MaxDigit(int num) // int num = number (создается копия передаваемого значения)
+{
+    int firstDigit = num / 10; 
+    int secondDigit = num % 10;
+    if (firstDigit > secondDigit) return firstDigit; // Если хдесь ложь, то переходим ниже
+    return secondDigit; // else здесь не нужен, т.к return прекращает работу метода
+}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа --> {maxDigit} ");
---
+++ Task016/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Написать программу, которая:
+1. Получает на вход два числа
+2. Проверяет, является ли первое число квадратом второго */
+
+Console.Write("Enter two numbers: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Square(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1; // || - или
+}
+
+bool result = Square(number1, number2);
+Console.WriteLine(result ? "Yes" : "No");
