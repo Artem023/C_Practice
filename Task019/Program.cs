32854ec4a30e442fd90a0f5f54c8093819567447
Artem Neliubov <artemneliubov@yandex.ru>
2022-11-21 16:48:40 +0300

    Added Tasks (019, 021, 023)

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NewNumber(num)
+{
+    int num5 = num % 10; //num5 * 10000
+    int num4 = num / 10 % 10; // num4 * 1000 + num5
+    int num3 = num / 100 % 10; // num3 * 100 + num4
+    int num2 = num / 1000 % 10; // num2 * 10 + num3
+    int num1 = num / 10000;
+    res = num5 * 10000 
+        + num4 * 1000 
+        + num3 * 100 
+        + num2 * 10
+        + num1
+}
+
+if (number > 9999 && number < 100000)
+{
+    int result = NewNumber(number);
+    if (result == number)
+    { 
+    Console.WriteLine($"{number} --> Yes");
+    }
+    else Console.WriteLine($ {number} --> "No");
+}
+else Console.WriteLine("WRONG NUMBER");
--- Task023/Program.cs
+++ Task023/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125 */
+
+Console.Write("Enter a number: ")
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int number)
+{
+    Console.WriteLine($"{number ->}")
+    int number1 = 1;
+    while (number1 <= number)
+    {
+        int result = number * number * number;
+        Console.WriteLine($"number1 | }");
+        Console.Write(result);
+        number1 ++;
+    }
+    
+}
+if (number > 0)
+{
+    Cube(num); 
+}
+else Console.WriteLine("ERROR");
