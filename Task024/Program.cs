9237cef1001ff37350696a5997c6fb79a493cb26
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-24 19:16:19 +0300

    Task 024 done

---
+++ Task024/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая
+1. Принимает число (А)
+2. Выдает сумму чисел от 1 до А */
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers (int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+    sum = sum + i;
+    }
+    return sum;
+}
+
+if (number > 0)
+{
+int sumnumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumnumbers}");
+}
+else Console.WriteLine("ERROR");
