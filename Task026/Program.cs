52f29859b6f141ec1c19be16a57e4cd13abff5d5
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-24 21:59:21 +0300

    Task 026 done

---
+++ Task026/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Написать программу, которая
+1. принимает число
+2. выдает количество цифр в числе */
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CountDigits(int num)
+{
+    int digit = 0;    
+    while (num != 0)
+    {
+        num = num / 10;
+        digit++;
+    }
+    return digit;   
+}
+
+int countdigits = CountDigits(number);
+Console.WriteLine($"{number} -> {countdigits}");
