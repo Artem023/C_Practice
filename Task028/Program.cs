04823051d286e6ed30802e99f065641474bc5220
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-27 12:57:47 +0300

    Task 028 done

---
+++ Task028/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая:
+1. принимает на вход число N
+2. выдает произведение чисел от 1 до N */
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MultNumbers (int num)
+{
+   int mult = 1;
+   for (int i = 1; i <= num; i++)
+   {
+   mult = mult * i;
+   }
+   return mult;
+}
+
+if (number > 0)
+{
+int result = MultNumbers(number);
+Console.WriteLine($"Произведение число от 1 до {number} = {result}");
+}
+else Console.WriteLine("Требуется ввести натерально число");
