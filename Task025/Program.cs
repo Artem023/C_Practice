7e25a1b0c7ab870fc42b2bac9ce65313e73edb79
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-27 14:19:05 +0300

    HW Task 025 done

---
+++ Task025/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите цикл, который 
+1. принимает на вход два числа (A и B) 
+2. возводит число A в натуральную степень B.
+3, 5 -> 243
+2, 4 -> 16 */
+
+Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void NumberDegree(int number1, int number2)
+{
+    int result = 1;
+    for (int i = 1; i <= number2; i ++)
+    {
+        result = result * number1;
+    }
+    Console.Write($"The number {number1} to the power of the number {number2} is {result} ");
+}
+
+NumberDegree(num1, num2);
