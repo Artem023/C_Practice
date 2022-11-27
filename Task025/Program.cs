6c3a0ad0858a8e7b1f0b82542dd9733156470565
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-27 14:24:59 +0300

    HW Task 025 modified

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -19,4 +19,8 @@ void NumberDegree(int number1, int number2)
     Console.Write($"The number {number1} to the power of the number {number2} is {result} ");
 }
 
+if (num2 > 0 && num1 > 0)
+{
 NumberDegree(num1, num2);
+}
+else Console.WriteLine("You enter wrong number");
