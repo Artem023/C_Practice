031efb9b5dc349c1f4577085b7523e3bbb04e000
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-28 19:05:16 +0300

    HW Task 027 done

---
+++ Task027/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая 
+1. принимает на вход число 
+2. выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+    
+int SumDigits (int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+if (number >= 0)
+{    
+    int result = SumDigits(number);
+    Console.Write($"{number} -> {result} ");
+}
+else Console.Write("Change your number");
